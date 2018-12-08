c1eb185160532c9921b4585bc2a44ce0b925660c
tweety <ekak9380@gmail.com>
2018-12-08 22:24:27 +0900

    movie,,02-05

--- main.c
+++ main.c
@@ -23,25 +23,20 @@ int main(int argc, char *argv[]) {
 	
 	//1. reading the movie.dat-----------------------------
 	
-	fp=fopen("movie.dat.dat","r");
+	fp=fopen("movie.dat.dat","r"); //영화 정보가 들어있는 dat 파일을 읽어온다. 
 	//1.1 FILE open
 	
 	
 	//1.2 list generation (use function list_genList() )
-	list = list_genList();
+	list = list_genList();//영화 정보를 담을 그릇(리스트) 
 	
 	//1.3 read each movie data from the file and add it to the linked list
-	while (EOF != fscanf(fp,"%s%s%d%f",name,country,&runTime,&score)/*수정한것 /* read name, country, runtime and score*/ )
-	{	
-	   void* mvInfo= mv_genMvInfo(name,score,runTime,country);
-	   //printMv(mvInfo);
-	   list_addTail(mvInfo, list);
+	while (EOF != fscanf(fp,"%s%s%d%f",name,country,&runTime,&score)/* read name, country, runtime and score*/ )
+	{	//fscanf로 파일 속 정보들을 받는다.
 	   
-	   //깃허브 10장 문자열 하나씩 읽어오는 거 참고./ 
-	    //movInfo_t();
-	    //mv_genMvInfo()
-	    //구조체 만들기 movinfo이용, 와플 찍는곳 
-	    //mv_genMvInfo()
+	   void* mvInfo= mv_genMvInfo(name,score,runTime,country);
+	   //mv_genMvInfo 에 받은 정보를 넣는다. 
+	   list_addTail(mvInfo, list);// 리스트 꼬리(뒤)에 계속 리스트를 추가한다. 
 
 	//generate a movie info instance(mvInfo) with function mv_genMvInfo()
 		
@@ -74,26 +69,26 @@ int main(int argc, char *argv[]) {
 				printf("\nprinting all the movies in the list.....\n\n\n");
 				printf("----------------------------------------\n");
 				
-				repFunc = mv_printAll;
-				arg = NULL;
+				repFunc = mv_printAll; 
+				arg = NULL; // 모든 영화 정보이기 때문에 특정 arg가 존재하지 않음. 
 			
 				break;
 				
 			case 2: //print movies of specific country
 			    printf("\nselect a country : ");
-			    scanf("%s",&country);
+			    scanf("%s",&country);// 출력하고자 하는 나라의 정보를 입력받는다.  
 			    
-			    repFunc = mv_printCountry;
-				arg = country; 
+			    repFunc = mv_printCountry; //모든 영화 정보중 나라 정보만 따짐  
+				arg = country;// 입력받은 정보(arg)는 여기서 country가 됨. 
 			    
 				break;
 				
 			case 3: //print movies with long runtime
 			    printf("\nlowest runTime : ");
-			    scanf("%d",&runTime);
+			    scanf("%d",&runTime);// 최소 상영시간을 입력받는다.  
 			    
-			    repFunc = mv_printRunTime;
-				arg = &runTime;
+			    repFunc = mv_printRunTime; //모든 영화 정보 중 상영시간에 대해서만 따짐  
+				arg = &runTime; //runTime은 int 형으로 받아야 하므로 &연산자를 써준다. 
 
 				break;
 				
@@ -101,25 +96,25 @@ int main(int argc, char *argv[]) {
 			    printf("\nlowest score : ");
 			    scanf("%f",&score);
 			    
-			    repFunc = mv_printScore;
-				arg = &score;
+			    repFunc = mv_printScore; // 모든 영화 정보 중 평점에 대해서만 따짐  
+				arg = &score; //score는 float형으로 받아야 하므로 &연산자를 써준다.  
 				
 				break;
 				
 			case 5:
 				printf("\n\nBye!\n\n");
-				exit_flag = 1;
+				exit_flag = 1; // 빠져나감. 
 				
 				break;
 				
 			default:
-				printf("wrong command! input again\n");
+				printf("wrong command! input again\n"); // 1~5 밖의 숫자 선택 시 다시 선택하도록 함. 
 				break;
 		}
 		
 		//list_repeatFunc(repFunc,arg,list); 
-		cnt=list_repeatFunc(repFunc,arg,list);
-		printf("\n  -totally %d movies are listed!\n\n\n",cnt);
+		cnt=list_repeatFunc(repFunc,arg,list); // list_repeatFunc은 각 리스트(각 영화)별로 반복하고 정보를 만족하는 영화만 거르는 것 이므로 cnt는 정보를 만족한 영화의 개수라고 할 수 있음. 
+		printf("\n  -totally %d movies are listed!\n\n\n",cnt); // 정보와 일치하는 영화 개수(cnt)도 출력함. 
 		//2.2 printing operation by function pointer (list_repeatFunc() is called here)
 		//2.3 print number of movies
 	}
--- movie.c
+++ movie.c
@@ -9,29 +9,29 @@ typedef struct movInfo {
 	float score;
 	int runTime;
 	char madeIn[10];
-} movInfo_t;
+} movInfo_t; //영화 정보 구조체  
 
-void* mv_genMvInfo(char* name, float score, int runTime, char* country)
+void* mv_genMvInfo(char* name, float score, int runTime, char* country) //구조체에 정보 넣어서 와플 찍는 과정. 
 {   
-	movInfo_t* mvPtr;
+	movInfo_t* mvPtr;   
 	
-	mvPtr = (movInfo_t*)malloc(sizeof(movInfo_t));
+	mvPtr = (movInfo_t*)malloc(sizeof(movInfo_t)); //malloc 함수를 이용해서 동적 메모리 할당. 
 	
 	if(mvPtr == NULL)
 	{
 		printf("Error\n");
 		return NULL;
-	}
+	} 
 	
-	strcpy(mvPtr->madeIn,country);
-	strcpy(mvPtr->name,name);
-	mvPtr->runTime=runTime;
+	strcpy(mvPtr->madeIn,country); //국가는 문자열이므로 문자열 복사를 이용해서 규조체 안에 정보를 전달한다. 
+	strcpy(mvPtr->name,name); //제목도 국가와 마찬가지로 문자열이므로 문자열 복사를 이용한다. 
+	mvPtr->runTime=runTime; //런타임과 스코어는 정수형 변수이므로 굳이 복사하지 않고 바로 구조체로 정보를 전달한다. 
 	mvPtr->score=score;
 	
-	// malloc 써야함  동적메모리 할당.  
+	  
 	//allocate memory and set the member variables
 	
-	return (void*)mvPtr;
+	return (void*)mvPtr; // void*형 mvptr를 반환해 준다.(영화 하나하나에 대해 구조체를 만들 것이므로 반환해 줘야함) 
 }
 
 void printMv(void* obj)
@@ -48,52 +48,52 @@ void printMv(void* obj)
 	return;
 }
 
-int mv_printAll(void* obj, void* arg)
+int mv_printAll(void* obj, void* arg) //모든 영화 정보를 출력하는 함수  
 {
-	printMv(obj);
+	printMv(obj);//obj 모두출력  
 	printf("------------------------------------\n");
 	
 	return 1; 
-	
-	//거르는 곳 
+
 }
 
-int mv_printScore(void* obj, void* arg)
+int mv_printScore(void* obj, void* arg) //입력받은 스코어와 연관 있는 영화 정보만 출력하는 함수  
 {
-	movInfo_t* mvPtr = (movInfo_t*)obj;
+	movInfo_t* mvPtr = (movInfo_t*)obj; //특정 정보(obj)에 대해서만 봄. 
 	
-	if( ((mvPtr-> score) >= *(float*)arg))
+	if( ((mvPtr-> score) >= *(float*)arg))// 입력받은 최소 평점(arg)보다 크거나 같은 score일 때  
 	{
-		printMv(obj);
+		printMv(obj);// 조건을 만족하는 obj만 출력  
 	    printf("------------------------------------\n");
-	    return 1;
+	    return 1; // 영화 하나씩 따질 것 이므로 만족하면 1로 반환해서 출력, 아닐경우 0으로 반환, 그냥 넘어감.  
 	}
 	
 	return 0;
 }
 
-int mv_printRunTime(void* obj, void* arg)
+int mv_printRunTime(void* obj, void* arg) //입력받은 런타임과  연관 있는 영화 정보만 출력하는 함수
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
 	
-	if((mvPtr-> runTime) >= *(int*) arg )
+	if((mvPtr-> runTime) >= *(int*) arg )// 입력받은 최소 상영시간 (arg)보다 크거나 같은 runTime일 때
 	{
-		printMv(obj);
+		printMv(obj); // 조건을 만족하는 obj만 출력 
 	    printf("------------------------------------\n");
-	    return 1;
+	    return 1; // 영화 하나씩 따질 것 이므로 만족하면 1로 반환해서 출력, 아닐경우 0으로 반환, 그냥 넘어감.
 	}
 	return 0;
 	
 }
 
-int mv_printCountry(void* obj, void* arg)
+int mv_printCountry(void* obj, void* arg) //입력받은 국가와  연관 있는 영화 정보만 출력하는 함수
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
 	
-	if(strcmp(mvPtr->madeIn, (char*)arg)==0){
-		printMv(obj);
+	if(strcmp(mvPtr->madeIn, (char*)arg)==0) //입력받은 국가(arg)와 같을 경우       
+	{
+		printMv(obj);// 조건을 만족하는 obj만 출력
 	    printf("------------------------------------\n");
-	    return 1;
+	    return 1;// 영화 하나씩 따질 것 이므로 만족하면 1로 반환해서 출력, 아닐경우 0으로 반환, 그냥 넘어감.
 	}
 	return 0;
 }
